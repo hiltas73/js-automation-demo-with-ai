801ffcadbf7903ed8108188ee260641dc999fd48
hiltas73 <h_iltas@yahoo.com>
2025-01-18 17:28:12 +0300

    Start Refund dates steps created with empty code

---
+++ steps/getting-started/startAndRefundDates.js
@@ -0,0 +1,23 @@
+import { Given, Then, When } from "@cucumber/cucumber";
+import { expect } from "@playwright/test";
+import { startApplicationPage, page } from "../../globalPagesSetup.js";
+import { productInfo } from "../../utilities/qa-data-reader.js";
+
+
+Given('user is on the enrollment page', function () {
+});
+
+Then('the program start date is displayed', function () {
+});
+
+Then('the program refund date is displayed', function () {
+});
+
+Given('user is on the enrollment page', function () {
+});
+
+Then('the displayed program start date is correct', function () {
+});
+
+Then('the displayed program refund date is correct', function () {
+});
